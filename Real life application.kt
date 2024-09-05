7d9b3f6436d0fe87bfc5d1dacc700dc7b610acd3
Ankita-12213031 <119611039+Ankita-12213031@users.noreply.github.com>
2024-09-05 23:18:59 +0530

    Add files via upload

---
+++ Real life application.kt
@@ -0,0 +1,19 @@
+fun main()
+ {
+     val purchase=100000
+     val hasmembership="n"
+     if(purchase>=1000)
+     {
+     if(hasmembership=="y"){
+         println("20% discount available")
+     }
+     else
+     {
+         println("10% discount")
+     }
+     }
+     else
+         {
+             println("No discount")
+         }
+     }
